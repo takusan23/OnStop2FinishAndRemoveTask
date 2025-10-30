47d34e08cce64f0e13b94631eef5459b41678130
takusan23 <takumu1200@gmail.com>
2025-10-31 01:52:01 +0900

    shizuku の付与を待つ

--- app/src/main/java/io/github/takusan23/onstop2finishandremovetask/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/onstop2finishandremovetask/MainActivity.kt
@@ -1,14 +1,20 @@
 package io.github.takusan23.onstop2finishandremovetask
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import io.github.takusan23.onstop2finishandremovetask.ui.screen.MainScreen
 import io.github.takusan23.onstop2finishandremovetask.ui.theme.OnStop2FinishAndRemoveTaskTheme
+import kotlinx.coroutines.launch
 import org.lsposed.hiddenapibypass.HiddenApiBypass
+import rikka.shizuku.Shizuku
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +31,20 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
-        // Foreground Service 開始
-        ContextCompat.startForegroundService(this, Intent(this, OnStop2FinishAndRemoveTaskService::class.java))
+        lifecycleScope.launch {
+
+            // Shizuku の権限付与まで待つ
+            suspendCoroutine { continuation ->
+                Shizuku.addRequestPermissionResultListener { _, grantResult ->
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        continuation.resume(Unit)
+                    }
+                }
+            }
+
+            // Foreground Service 開始
+            ContextCompat.startForegroundService(this@MainActivity, Intent(this@MainActivity, OnStop2FinishAndRemoveTaskService::class.java))
+        }
     }
 
 }
