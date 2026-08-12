6f5ad2f875dbd72e3d20afd46afb7cfe6e4a3e81
takusan23 <takumu1200@gmail.com>
2026-08-12 21:30:39 +0900

    説明文を追記、service を exported=false に、specialUse の説明を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.takusan23.onstop2finishandremovetask"
         minSdk = 31
         targetSdk = 37
-        versionCode = 5
-        versionName = "2.0.3"
+        versionCode = 6
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
